2efec543c41b1a2ad3e8c3ea27751a4394cb833b
Tijana Savic <tijanasavic1984@gmail.com>
2022-01-13 18:06:39 +0100

    added math functions

---
+++ main.js
@@ -0,0 +1,36 @@
+const sum = function(a, b) {
+  return a + b;
+}
+console.log("sum", sum(65, 4));
+
+const substract  = function(a, b) {
+  return a - b;
+}
+console.log("substract ", substract(50, 15));
+
+const divide  = function(a, b) {
+  return a / b;
+}
+console.log("divide ", divide(25, 5));
+
+const myltiply = function(a, b) {
+  return a * b;
+}
+console.log("myltiply", myltiply(4, 5));
+
+const sqrt = function(a) {
+  return Math.sqrt(a);
+}
+console.log("sqrt", sqrt(16));
+
+const  round = function(a,b) {
+  return Math.round(a,b);
+}
+console.log("round", round(2));
+
+const pow = function(a) {
+  return Math.pow(a, 2);
+}
+console.log("pow", pow(5));
+
+
